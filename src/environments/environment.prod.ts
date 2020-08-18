d7696596fcdc528316982c740dcab4d7bd9b947d
Brad Wheeler <brad@launchagent.co.nz>
2020-08-18 14:38:04 +1200

    init and nav setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAtlPgx5VbKYk4_4u6J4C3VVM_0qSupHN8",
+    authDomain: "kanban-app-b8e51.firebaseapp.com",
+    databaseURL: "https://kanban-app-b8e51.firebaseio.com",
+    projectId: "kanban-app-b8e51",
+    storageBucket: "kanban-app-b8e51.appspot.com",
+    messagingSenderId: "583745493450",
+    appId: "1:583745493450:web:a32d4ffe83f22f5ec29733",
+    measurementId: "G-C59JZ7E90W"
+  }
 };
