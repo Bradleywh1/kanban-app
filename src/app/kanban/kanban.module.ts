f29b01f8905d69225bed145e3918fa70a0db0008
Brad Wheeler <brad@launchagent.co.nz>
2020-08-27 16:45:35 +1200

    working board list with drag n drop

--- src/app/kanban/kanban.module.ts
+++ src/app/kanban/kanban.module.ts
@@ -8,10 +8,12 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDialogModule } from '@angular/material/dialog';
 import { BoardListComponent } from './board-list/board-list.component';
+import { BoardComponent } from './board/board.component';
+import { BoardDialogComponent } from './dialogs/board-dialog.component';
 
 
 @NgModule({
-  declarations: [BoardListComponent],
+  declarations: [BoardListComponent, BoardComponent, BoardDialogComponent],
   imports: [
     CommonModule,
     KanbanRoutingModule,
@@ -20,6 +22,7 @@ import { BoardListComponent } from './board-list/board-list.component';
     DragDropModule,
     MatDialogModule,
     MatButtonToggleModule,
-  ]
+  ],
+  entryComponents: [BoardDialogComponent]
 })
 export class KanbanModule { }
