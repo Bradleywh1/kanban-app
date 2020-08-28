8706b9c46b5a1112d0bf5af66daa1fbdd3e7fd62
Brad Wheeler <brad@launchagent.co.nz>
2020-08-28 17:19:42 +1200

    working kanban

--- src/app/kanban/dialogs/board-dialog.component.ts
+++ src/app/kanban/dialogs/board-dialog.component.ts
@@ -23,9 +23,13 @@ import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 })
 export class BoardDialogComponent {
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<BoardDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
 
-  ngOnInit(): void {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
